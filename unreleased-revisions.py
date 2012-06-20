cdb39aa03aa74590646776cc886a34a577f321d2
Michael Hudson-Doyle <michael.hudson@linaro.org>
2012-06-20 12:34:07 +1200

    rewrite unreleased-revisions to be a bit more modular

--- unreleased-revisions.py
+++ unreleased-revisions.py
@@ -4,43 +4,72 @@ import bzrlib.errors
 import os
 import sys
 
-branches = os.listdir('/srv/lava/branches')
 
-if sys.argv[1:]:
-    branches = sys.argv[1:]
+class Component(object):
 
-for branch_name in sorted(branches):
-    print branch_name,
-    branch = bzrlib.branch.Branch.open(
-        os.path.join('/srv/lava/branches', branch_name))
-    branch.lock_read()
-    try:
-        tags = branch.tags.get_tag_dict()
-        released_revnos = []
-        for tag, revid in tags.iteritems():
-            if tag.startswith('release-'):
-                try:
-                    revno = branch.revision_id_to_dotted_revno(revid)
-                except bzrlib.errors.NoSuchRevision:
-                    pass
-                else:
-                    released_revnos.append((revno, tag[len('release-'):]))
-        released_revno, release = max(released_revnos)
-        print release,
-        revid = branch.last_revision()
-        unreleased_revs = []
-        while True:
-            rev = branch.repository.get_revision(revid)
-            revno = branch.revision_id_to_dotted_revno(rev.revision_id)
-            if revno <= released_revno:
-                break
-            unreleased_revs.append((rev, revno))
-            revid = rev.parent_ids[0]
-        if unreleased_revs:
-            print len(unreleased_revs), 'unreleased revisions'
-            for rev, revno in unreleased_revs:
-                print ' ', revno[0], rev.message.splitlines()[0][:100]
-        else:
-            print ' no unreleased revisions'
-    finally:
-        branch.unlock()
+    def __init__(self, name):
+        self.name = name
+        self.last_release = None
+        self.tip_revno = None
+        self.unreleased_revisions = None
+
+
+def load_manifest(fname):
+    d = {}
+    for line in open(fname):
+        line = line.strip()
+        if line.startswith('#'):
+            package, version = line.split('==')
+        d[package.lower().strip()] = version.strip()
+    return d
+
+
+def create_components_from_branches(branches=None):
+
+    if branches is None:
+        branches = os.listdir('/srv/lava/branches')
+
+    components = {}
+
+    for branch_name in branches:
+        component = Component(branch_name)
+        branch = bzrlib.branch.Branch.open(
+            os.path.join('/srv/lava/branches', branch_name))
+        branch.lock_read()
+        try:
+            tags = branch.tags.get_tag_dict()
+            released_revnos = []
+            for tag, revid in tags.iteritems():
+                if tag.startswith('release-'):
+                    try:
+                        revno = branch.revision_id_to_dotted_revno(revid)
+                    except bzrlib.errors.NoSuchRevision:
+                        pass
+                    else:
+                        released_revnos.append((revno, tag[len('release-'):]))
+            released_revno, release = max(released_revnos)
+            component.last_release = release
+            revid = branch.last_revision()
+            unreleased_revs = []
+            while True:
+                rev = branch.repository.get_revision(revid)
+                revno = branch.revision_id_to_dotted_revno(rev.revision_id)
+                if revno <= released_revno:
+                    break
+                if rev.message != 'post release bump':
+                    unreleased_revs.append((rev, revno))
+                revid = rev.parent_ids[0]
+            component.unreleased_revisions = unreleased_revs
+        finally:
+            branch.unlock()
+        components[branch_name] = component
+
+    return components
+
+
+if __name__ == '__main__':
+    components = create_components_from_branches()
+    for name, component in sorted(components.items()):
+        print name, component.last_release, len(component.unreleased_revisions)
+        for rev, revno in component.unreleased_revisions:
+            print ' ', revno[0], rev.message.splitlines()[0][:100]
