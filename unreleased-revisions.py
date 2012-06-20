c96f3011a23250c2d2a993719612975e22e29975
Michael Hudson-Doyle <michael.hudson@linaro.org>
2012-06-20 15:27:24 +1200

    show revs between installed release and latest

--- unreleased-revisions.py
+++ unreleased-revisions.py
@@ -6,7 +6,6 @@ import bzrlib.errors
 from twisted.web.template import CDATA, flattenString, tags
 
 import os
-import sys
 
 
 def flatten(t):
@@ -51,7 +50,8 @@ def create_components_from_branches(branches=None):
         branch.lock_read()
         try:
             branch_tags = branch.tags.get_tag_dict()
-            released_revnos = []
+            revno2release = {}
+            release2revno = {}
             for tag, revid in branch_tags.iteritems():
                 if tag.startswith('release-'):
                     try:
@@ -59,20 +59,29 @@ def create_components_from_branches(branches=None):
                     except bzrlib.errors.NoSuchRevision:
                         pass
                     else:
-                        released_revnos.append((revno, tag[len('release-'):]))
-            released_revno, release = max(released_revnos)
-            component.last_release = release
-            revid = branch.last_revision()
+                        if len(revno) > 1:
+                            continue
+                        revno2release[revno[0]] = tag[len('release-'):]
+                        release2revno[tag[len('release-'):]] = revno[0]
+            component.released_revno = max(revno2release)
+            component.last_release = revno2release[max(revno2release)]
+            component.release2revno = release2revno
+            component.revno2release = revno2release
+            revno, revid = branch.last_revision_info()
+            mainline_revs = []
             unreleased_revs = []
             while True:
                 rev = branch.repository.get_revision(revid)
-                revno = branch.revision_id_to_dotted_revno(rev.revision_id)
-                if revno <= released_revno:
-                    break
                 if rev.message != 'post release bump':
-                    unreleased_revs.append((rev, revno))
+                    if revno > component.released_revno:
+                        unreleased_revs.append((rev, revno))
+                    mainline_revs.append((rev, revno))
+                if not rev.parent_ids:
+                    break
                 revid = rev.parent_ids[0]
+                revno -= 1
             component.unreleased_revisions = unreleased_revs
+            component.mainline_revs = mainline_revs
         finally:
             branch.unlock()
         components[branch_name] = component
@@ -138,7 +147,7 @@ def format_revlist(revlist):
     revs = tags.table()
     for rev, revno in revlist:
         r = tags.tr()
-        r(tags.td(str(revno[0])))
+        r(tags.td(str(revno)))
         r(tags.td(rev.message.splitlines()[0][:100],
                   style="text-align: left"))
         revs(r)
@@ -168,9 +177,9 @@ def make_html(components, instances):
         unreleased_count = len(component.unreleased_revisions)
         if unreleased_count:
             id_ = get_id()
-            content = (
-                tags.a(str(unreleased_count), href='#', class_='highlight'),)
-            td(content, class_='version clickable', id=id_)
+            td(
+                tags.a(str(unreleased_count), href='#', class_='highlight'),
+                class_='version clickable', id=id_)
             extra_rows.append(
                 tags.tr(
                     tags.td(
@@ -180,7 +189,7 @@ def make_html(components, instances):
                     id="show-" + id_))
         else:
             td(str(unreleased_count), class_='version')
-        td(component.last_release, class_='version')
+        td(component.last_release, ' ', str(component.released_revno), class_='version')
         for instance_name, instance in sorted(instances.items()):
             ver = instance.get(name)
             if ver is None:
@@ -188,7 +197,25 @@ def make_html(components, instances):
             elif ver == component.last_release:
                 td(ver, class_='version')
             else:
-                td(tags.a(ver, href='#'), class_='version highlight')
+                id_ = get_id()
+                td(
+                    tags.a(ver, href='#', class_='highlight'),
+                    class_='version clickable', id=id_)
+                if 'dev' in ver:
+                    revno_low = int(ver.split('dev')[1])
+                else:
+                    revno_low = component.release2revno[ver]
+                revlist = []
+                for rev, revno in component.mainline_revs:
+                    if revno_low < revno < component.released_revno:
+                        revlist.append((rev, revno))
+                extra_rows.append(
+                    tags.tr(
+                        tags.td(
+                            format_revlist(revlist),
+                            colspan=str(3 + len(instances))),
+                        class_='hidden',
+                        id="show-" + id_))
         tbody(row, *extra_rows)
     table(tbody)
     html = tags.html(
